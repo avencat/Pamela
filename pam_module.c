2fb24244b2a1d0060caf191866493912f7a8604a
Axel Vencatareddy <avencatareddy@me.com>
2016-10-18 20:47:54 +0200

    pam_module updated with all necessary functions

--- pam_module.c
+++ pam_module.c
@@ -8,5 +8,54 @@
 
 PAM_EXTERN int  pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-  return (0);
+  (void)pamh;
+  (void)flags;
+  (void)argc;
+  (void)argv;
+  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int  pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  (void)pamh;
+  (void)flags;
+  (void)argc;
+  (void)argv;
+  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int  pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  (void)pamh;
+  (void)flags;
+  (void)argc;
+  (void)argv;
+  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int  pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  (void)pamh;
+  (void)flags;
+  (void)argc;
+  (void)argv;
+  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int  pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  (void)pamh;
+  (void)flags;
+  (void)argc;
+  (void)argv;
+  return (PAM_SUCCESS);
+}
+
+PAM_EXTERN int  pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+  (void)pamh;
+  (void)flags;
+  (void)argc;
+  (void)argv;
+  return (PAM_SUCCESS);
 }
