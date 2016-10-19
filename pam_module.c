a2a5fcce457ff2f7dc2a370be0d9e879a9fcf458
Axel Vencatareddy <avencatareddy@me.com>
2016-10-19 19:53:26 +0200

    pam_module updated

--- pam_module.c
+++ pam_module.c
@@ -28,6 +28,7 @@ PAM_EXTERN int	pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
   int		retval;
   char  *pass;
   const char	*pUsername;
+  char userDir[256];
 
   puts("In Authenticate");
   retval = pam_get_user(pamh, &pUsername, "Username: ");
@@ -35,9 +36,13 @@ PAM_EXTERN int	pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
   if (retval != PAM_SUCCESS) {
     return (retval);
   }
+  strcpy(userDir, "/home/");
+  strcat(userDir, pUsername);
   pam_get_item(pamh, PAM_AUTHTOK, (const void **)&pass);
   printf("pass = \"%s\"\n", pass);
   pam_set_data(pamh, "pam_module", strdup(pass), &cleanup);
+  pam_set_data(pamh, "pam_module_user_dir", strdup(userDir), &cleanup);
+  pam_set_data(pamh, "pam_module_user", strdup(pUsername), &cleanup);
   (void)flags;
   (void)argc;
   (void)argv;
@@ -65,10 +70,23 @@ PAM_EXTERN int  pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const 
 PAM_EXTERN int  pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
   char  *pass;
+  char  *userDir;
+  char  *user;
+  char  cmd[1024];
+  char  tmp[256];
 
   puts("In open session");
   pam_get_data(pamh, "pam_module", (const void **)&pass);
+  pam_get_data(pamh, "pam_module_user_dir", (const void **)&userDir);
+  pam_get_data(pamh, "pam_module_user", (const void **)&user);
   printf("pass = \"%s\"\n", pass);
+  strcpy(tmp, "cipher");
+  strcat(tmp, user);
+  strcpy(cmd, "sudo cryptsetup luksOpen /home/");
+  strcat(cmd, tmp);
+  strcat(cmd, " ");
+  strcat(cmd, tmp);
+  system(cmd);
   (void)pamh;
   (void)flags;
   (void)argc;
