af20763c03258572abc3cb6f4c9c47a96ca5f7d8
Axel Vencatareddy <avencatareddy@me.com>
2016-10-22 17:36:02 +0200

    It's working when you already created the containers

--- pam_module.c
+++ pam_module.c
@@ -26,22 +26,15 @@ void  cleanup(pam_handle_t *pamh, void *data, int error_status)
 PAM_EXTERN int	pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
   int		retval;
-  char  *pass;
+  char		*pass;
   const char	*pUsername;
-  char userDir[256];
 
-  puts("In Authenticate");
   retval = pam_get_user(pamh, &pUsername, "Username: ");
-  printf("Welcome %s\n", pUsername);
   if (retval != PAM_SUCCESS) {
     return (retval);
   }
-  strcpy(userDir, "/home/");
-  strcat(userDir, pUsername);
   pam_get_item(pamh, PAM_AUTHTOK, (const void **)&pass);
-  printf("pass = \"%s\"\n", pass);
-  pam_set_data(pamh, "pam_module", strdup(pass), &cleanup);
-  pam_set_data(pamh, "pam_module_user_dir", strdup(userDir), &cleanup);
+  pam_set_data(pamh, "pam_module_pass", strdup(pass), &cleanup);
   pam_set_data(pamh, "pam_module_user", strdup(pUsername), &cleanup);
   (void)flags;
   (void)argc;
@@ -67,27 +60,47 @@ PAM_EXTERN int  pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const 
   return (PAM_SUCCESS);
 }
 
+char	*firstInCapital(char *str)
+{
+  char	*ret;
+
+  if (!str || !str[0] || (ret = malloc(strlen(str))) == NULL)
+    return (ret);
+  strcpy(ret, str);
+  if (ret[0] <= 'z' && ret[0] >= 'a')
+    ret[0] += 'A' - 'a';
+  return (ret);
+}
+
 PAM_EXTERN int  pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
   char  *pass;
-  char  *userDir;
   char  *user;
+  char	*capUser;
   char  cmd[1024];
-  char  tmp[256];
+  char  cipherFileName[256];
 
-  puts("In open session");
-  pam_get_data(pamh, "pam_module", (const void **)&pass);
-  pam_get_data(pamh, "pam_module_user_dir", (const void **)&userDir);
+  pam_get_data(pamh, "pam_module_pass", (const void **)&pass);
   pam_get_data(pamh, "pam_module_user", (const void **)&user);
-  printf("pass = \"%s\"\n", pass);
-  strcpy(tmp, "cipher");
-  strcat(tmp, user);
-  strcpy(cmd, "sudo cryptsetup luksOpen /home/");
-  strcat(cmd, tmp);
+  strcpy(cipherFileName, "cipher");
+  capUser = firstInCapital(user);
+  strcpy(cmd, "echo \"");
+  strcat(cmd, pass);
+  strcat(cmd, "\" | sudo cryptsetup luksOpen /home/");
+  strcat(cmd, user);
+  strcat(cmd, "/");
+  strcat(cipherFileName, capUser);
+  strcat(cmd, cipherFileName);
   strcat(cmd, " ");
-  strcat(cmd, tmp);
+  strcat(cmd, cipherFileName);
+  strcat(cmd, " && sudo mount /dev/mapper/");
+  strcat(cmd, cipherFileName);
+  strcat(cmd, " /home/");
+  strcat(cmd, user);
+  strcat(cmd, "/cipher");
   system(cmd);
-  (void)pamh;
+  if (capUser)
+    free(capUser);
   (void)flags;
   (void)argc;
   (void)argv;
@@ -96,8 +109,18 @@ PAM_EXTERN int  pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, con
 
 PAM_EXTERN int  pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-  puts("In close session");
-  (void)pamh;
+  char	*user;
+  char	cmd[256];
+
+  pam_get_data(pamh, "pam_module_user", (const void **)&user);
+  strcpy(cmd, "sudo umount /home/");
+  strcat(cmd, user);
+  strcat(cmd, "/cipher ; sudo cryptsetup luksClose cipher");
+  user = firstInCapital(user);
+  strcat(cmd, user);
+  system(cmd);
+  if (user)
+    free(user);
   (void)flags;
   (void)argc;
   (void)argv;
